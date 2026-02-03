c2adb9c03aa1af52564e1682e9ee14a8e6712f82
Aweem Ashar Bhatti <aweemashar@gmail.com>
2026-02-03 13:24:35 +0500

    Restructure Ts Files

--- api/domains.ts
+++ api/domains.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getRecentDomains } from './_lib/storage';
+import { getRecentDomains } from './_lib/storage.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'GET') {
