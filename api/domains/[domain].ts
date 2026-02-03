0881d055e957e0026097bfeb8886526775459096
Aweem Ashar Bhatti <aweemashar@gmail.com>
2026-02-03 14:56:31 +0500

    fix domain

--- api/domains/[domain].ts
+++ api/domains/[domain].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getDomain } from '../_lib/storage';
+import { getDomain } from '../_lib/storage.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'GET') {
